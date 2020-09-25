1f65c753ff4e7dde7f617ae1bf0b95c00a027003
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-09-25 01:32:37 -0500

    Adds function with user input

--- BoxBoundaries/BoxBoundaries/Source.cpp
+++ BoxBoundaries/BoxBoundaries/Source.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 
+
 struct Rect
 {
 	float minX;
@@ -12,25 +13,58 @@ struct Rect
 	float maxY;
 };
 
+float DistanceToRect(float x, float y, Rect& rect);
+
 int main()
 {
 	Rect rectangle = { 0, 0, 10, 10 };
-	std::cout << DistanceToRect(10, 10, rectangle);
+	float x = 0;
+	float y = 0;
+	std::cout << "Please enter Point X: ";
+	std::cin >> x;
+	std::cout << "\nPlease enter point Y: ";
+	std::cin >> y;
+	std::cout << "\nDistance from point to rectangle is " << DistanceToRect(x, y, rectangle) << std::endl;
 
+	system("PAUSE");
 	return 0;
 }
 
 float DistanceToRect(float x, float y, Rect& rect)
 {
-	// if the point is smaller than the mins then it is to the left or below
+	if (x > rect.maxX && y > rect.maxY)
+	{
+		// if the point is bigger than the maxs
+		// min < max < x
+		float a = x - rect.maxX;
+		float b = y - rect.maxY;
+		return sqrt((a*a) + (b*b));
+	}
+	else if (x > rect.maxX && y < rect.minY)
+	{
+		float a = x - rect.maxX;
+		float b = rect.minY - y;
+		return sqrt((a*a) + (b*b));
+	}
+	else if (x < rect.minX && y > rect.maxY)
+	{
+		float a = rect.minX - x;
+		float b = y - rect.maxY;
+		return sqrt((a*a) + (b*b));
+	}
+	else if (x < rect.minX && y < rect.minY)
+	{
+		// if the point is smaller than the mins
+		// x < mix < max
+		float a = rect.minX - x;
+		float b = rect.minY - y;
+		return sqrt((a*a) + (b*b));
+	}
+	else
+	{
+		// If point is inside box
+		return 0;
+	}
 
-	// x - rect.minX
-	// y - rect.minY
-
-	// if the point is bigger than the maxs then it is to the right or above
-
-
-
-
-	// a^2 + b^2 = c^2 for finding the distance
+	
 }
